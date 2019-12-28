dffd78eaba9db3cd63e83e0deb6b67a28a791377
HyoSung Bidol-Lee <hyosung11@me.com>
2019-12-27 18:28:00 -0800

    add describe()

--- script.test.js
+++ script.test.js
@@ -7,11 +7,22 @@ dbMock = [
   'dogpictures.com'
 ];
 
-it('this is a test', () => {
-  expect('hello').toBe('hello')
-})
+describe('googleSearch', () => {
+  it('this is a test', () => {
+    expect('hello').toBe('hello')
+  })
 
-it('is searching google', () => {
-  expect(googleSearch('testtest', dbMock)).toEqual([])
-  expect(googleSearch('dog', dbMock)).toEqual(['dog.com', 'dogpictures.com'])
+  it('is searching google', () => {
+    expect(googleSearch('testtest', dbMock)).toEqual([])
+    expect(googleSearch('dog', dbMock)).toEqual(['dog.com', 'dogpictures.com'])
+  })
+
+  it('work with undefined and null input', () => {
+    expect(googleSearch(undefined, dbMock)).toEqual([])
+    expect(googleSearch(null, dbMock)).toEqual([])
+  })
+
+  it('returns 3 or less matches', () => {
+    expect(googleSearch('.com', dbMock).length).toEqual(3)
+  })
 })
